d765a4a08d48734761c2a76d10d0f973e0e11775
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-20 14:30:30 -0400

    basic style updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import './App.scss'
+import gptLogo from './assets/chatgpt.svg'
 
 const App = () => {
   return (
     <div className="App">
       <div className="sidebar">
         <div className="upperSide">
+
           <div className="upperSideTop">
-            <img className="logo" src="" alt="" />
+            <img className="logo" src={gptLogo} alt="Logo" />
             <span className="brand">ChatGPT</span>
-            <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat</button>
-            <div className="upperSideBottom">
-              <button className="query"><img src="" alt="" />What is Programming?</button>
-              <button className="query"><img src="" alt="" />What is Programming?</button>
-            </div>
+          </div>
+
+          <button className="midBtn"><img src="" alt="" className="addBtn" />New Chat</button>
+
+          <div className="upperSideBottom">
+            <button className="query"><img src="" alt="" />What is Programming?</button>
+            <button className="query"><img src="" alt="" />What is Programming?</button>
           </div>
         </div>
+
         <div className="lowerSide">
 
         </div>
-      </div>
-      <div className="main">
-
-      </div>
     </div>
+
+    <div className="main">
+
+    </div>
+  </div>
   )
 }
 
