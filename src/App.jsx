a511200d23e2d2b9d80c3152de2d2a58427b7f4d
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-21 11:18:00 -0400

    layout change start

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,62 @@
 import './App.scss'
-import gptLogo from './assets/chatgpt.svg'
-import addBtn from './assets/add-30.png'
-import msgIcon from './assets/message.svg'
-import home from './assets/home.svg'
-import saved from './assets/bookmark.svg'
-import rocket from './assets/rocket.svg'
-import sendBtn from './assets/send.svg'
-import userIcon from './assets/user-icon.png'
-import gptImageLogo from './assets/chatgptLogo.svg'
-import useContext from 'react'
-import {Context} from './context/context'
+import {assets} from './assets/assets'
 
 const App = () => {
 
-  const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context)
 
   return (
     <div className="App">
-      <div className="sidebar"> 
+      {/* <div className="sidebar">
         <div className="upperSide">
 
           <div className="upperSideTop">
-            <img className="logo" src={gptLogo} alt="Logo" />
+            <img className="logo" src= alt="Logo" />
             <span className="brand">ChatGPT</span>
           </div>
 
-          <button className="midBtn"><img src={addBtn} alt="" className="addBtn" />New Chat</button>
+          <button className="midBtn"><img src= alt="" className="addBtn" />New Chat</button>
 
           <div className="upperSideBottom">
-            <button className="query"><img src={msgIcon} alt="Query" />What is Programming?</button>
-            <button className="query"><img src={msgIcon} alt="Query" />How do you use an API?</button>
+            <button className="query"><img src= alt="Query" />What is Programming?</button>
+            <button className="query"><img src= alt="Query" />How do you use an API?</button>
           </div>
         </div>
 
         <div className="lowerSide">
-          <div className="listItems"><img src={home} alt="Home" className="listItemsImg" />Home</div>
-          <div className="listItems"><img src={saved} alt="Saved" className="listItemsImg" />Saved</div>
-          <div className="listItems"><img src={rocket} alt="Upgrade" className="listItemsImg" />Upgrade to Pro</div>
+          <div className="listItems"><img src= alt="Home" className="listItemsImg" />Home</div>
+          <div className="listItems"><img src= alt="Saved" className="listItemsImg" />Saved</div>
+          <div className="listItems"><img src= alt="Upgrade" className="listItemsImg" />Upgrade to Pro</div>
         </div>
-    </div>
+    </div> */}
 
     <div className="main">
 
-      <div className="chats">
-        <div className="chat">
-          <img src={userIcon} alt="" className="userImage" />
-          <p className="txt">Example TextExample TextExample TextExample TextExample TextExample TextExample TextExample TextExample TextExample Text</p>
-        </div>
-        <div className="chat bot">
-          <img src={gptImageLogo} alt="" />
-          <p className="txt">Example TextExample TextExample TextExample TextExample TextExample TextExample TextExample TextExample TextExample Text</p>
-        </div>
+      <div className="nav">
+        <p>Gemini</p>
+        <img className="user-icon" src={assets.user_icon} alt="" />
       </div>
-
-      <div className="chatFooter">
-        <div className="inp">
-          <input type="text"  placeholder="Send a message..." /><button className="send" ><img  src={sendBtn} alt="Send" /></button>
-        </div>
-        <p>ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT August 20 version </p>
-
+      <div className="main-container">
+        <p><span>Hello, Dev.</span></p>
+        <p>How can I help you today?</p>
       </div>
-
+        <div className="cards">
+          <div className="card">
+            <p>Suggets beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning.</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code.</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
     </div>
   </div>
   )
