2a7ac3ae0b1f47b080baf0dbd1610f6ab9695322
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-21 19:34:23 -0400

    separate each line of code in output

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -14,6 +14,10 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState("")
 
+    const delayPara = (index, nextWork) => {
+
+    }
+
 
     const onSent = async(prompt) => {
 
@@ -22,7 +26,17 @@ const ContextProvider = (props) => {
         setShowResult(true)
         setRecentPrompt(input)
         const response = await run(input)
-        setResultData(response)
+        let responseArray = response.split("**")
+        let newResponse;
+        for(let i=0; i < responseArray.length; i++){
+            if(i === 0 || i%2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" +responseArray[i] + "</b>"
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br> </br>")
+        setResultData(newResponse2)
         setLoading(false)
         setInput("")
     }
